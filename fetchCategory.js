3382b376da7e9efae22ddd267c26da8a3f50970e
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-02-24 19:40:52 +0530

    modular header8

--- fetchCategory.js
+++ fetchCategory.js
@@ -1,6 +1,6 @@
 // fetchCategories.js
 async function fetchCategories() {
-    const categoriesResponse = await fetch('http://http://0.0.0.0/:5000/getCategories', { method: 'GET' });
+    const categoriesResponse = await fetch('http://13.211.236.206:5000/getCategories', { method: 'GET' });
     const categories = await categoriesResponse.json();
     sessionStorage.setItem('categories', JSON.stringify(categories));
     displayCategories(categories);
