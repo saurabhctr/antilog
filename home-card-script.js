2c2aad3ba424657ddd3c9b9536dfc0acb7b3ae8e
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-02-28 00:23:42 +0530

    modular header8

--- home-card-script.js
+++ home-card-script.js
@@ -7,7 +7,8 @@ $(document).ready(function () {
 
         // Make AJAX request to the API
         $.ajax({
-            url: `http://3.26.226.77:5000/getCards?noOfCard=${noOfCards}&type=${cardType}&filters=${JSON.stringify(filters)}`,
+            url: `http://3.26.226.77:5000/getCards`,
+            /*?noOfCard=${noOfCards}&type=${cardType}&filters=${JSON.stringify(filters)}*/
             type: 'GET',
             success: function (response) {
                 displayCards(response.cards);
