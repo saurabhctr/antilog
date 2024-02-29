0ce0ffdf9f5b414795e50571113382b58fadbb88
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-03-01 00:24:05 +0530

    set ip2 add stary bg new files particlesjs-config.json and included stary scripts with transparent background to fit in

--- click-handler.js
+++ click-handler.js
@@ -17,57 +17,24 @@ $(document).ready(function () {
         // Add a class to indicate the enlarged state
         enlargedImageContainer.addClass('enlarged');
 
-        // Remove the class and play restore sound when clicked again
+        // Remove the container after the transition
+        setTimeout(function () {
+            // Remove the 'enlarged' class only if it hasn't been clicked again
+            if (!enlargedImageContainer.hasClass('clicked-again')) {
+                restoreSound.play();
+                enlargedImageContainer.removeClass('enlarged');
+                // Remove the container after the restore sound is played
+                setTimeout(function () {
+                    enlargedImageContainer.remove();
+                }, restoreSound.duration * 1000);
+            }
+        }, (enlargedImageContainer.hasClass('enlarged') ? 0 : 500)); // Add 500ms delay if the container is in enlarged state
+
+        // Handle a second click on the enlarged image
         enlargedImage.click(function () {
-            // Check if the element has the 'enlarged' class
             if (enlargedImageContainer.hasClass('enlarged')) {
-                // Play restore sound
-                restoreSound.play();
-                // Remove the enlarged class after the restore sound is played
-                setTimeout(function () {
-                    enlargedImageContainer.removeClass('enlarged');
-                }, restoreSound.duration * 1000);
+                enlargedImageContainer.addClass('clicked-again');
             }
         });
-
-        // Remove the container after the transition
-        setTimeout(function () {
-            enlargedImageContainer.remove();
-        }, (enlargedImageContainer.hasClass('enlarged') ? 500 : 0)); // Add 500ms delay if the container is in enlarged state
-    });
-
-    // Handle click for text description divs
-    $('body').on('click', 'div.popupable', function () {
-        const textContent = $(this).text();
-        const enlargedContent = $('<div>').addClass('enlarged-content');
-        const closeButton = $('<span>').addClass('close-button').text('✖');
-
-        enlargedContent.text(textContent);
-        enlargedContent.append(closeButton);
-        $('body').append(enlargedContent);
-
-        // Play click sound
-        clickSound.play();
-
-        // Add a class to indicate the enlarged state
-        enlargedContent.addClass('enlarged');
-
-        // Remove the class and play restore sound when clicked again
-        closeButton.click(function () {
-            // Check if the element has the 'enlarged' class
-            if (enlargedContent.hasClass('enlarged')) {
-                // Play restore sound
-                restoreSound.play();
-                // Remove the enlarged class after the restore sound is played
-                setTimeout(function () {
-                    enlargedContent.removeClass('enlarged');
-                }, restoreSound.duration * 1000);
-            }
-        });
-
-        // Remove the container after the transition
-        setTimeout(function () {
-            enlargedContent.remove();
-        }, (enlargedContent.hasClass('enlarged') ? 500 : 0)); // Add 500ms delay if the container is in enlarged state
     });
 });
