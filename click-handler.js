7dfaabff6b10ac012a526cfe0a221989860aa6e2
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-02-29 23:57:51 +0530

    set ip2 add stary bg new files particlesjs-config.json and included stary scripts with transparent background to fit in

--- click-handler.js
+++ click-handler.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     const clickSound = new Audio('resources/audio/deep_click_sound.mp3');
-    const restoreSound = new Audio('resources/audio/fading_air_sound.mp3');
+    const restoreSound = new Audio('path/audio/fading_air_sound.mp3');
 
     // Handle click for images
     $('body').on('click', 'img.enlargeable', function () {
+        clickSound.play(); // Play click sound
+
         const imageUrl = $(this).attr('src');
         const enlargedImageContainer = $('<div>').addClass('enlarged-image-container');
         const enlargedImage = $('<img>').addClass('enlarged-image').attr('src', imageUrl).attr('alt', 'Enlarged Image');
@@ -11,25 +13,16 @@ $(document).ready(function () {
         enlargedImageContainer.append(enlargedImage);
         $('body').append(enlargedImageContainer);
 
-        // Play click sound
-        clickSound.play();
-
-        // Add a class to indicate the enlarged state
-        enlargedImageContainer.addClass('enlarged');
-
-        // Remove the class and play restore sound when clicked again
         enlargedImageContainer.click(function () {
-            if (enlargedImageContainer.hasClass('enlarged')) {
-                // Play restore sound
-                restoreSound.play();
-                // Remove the enlarged class
-                enlargedImageContainer.removeClass('enlarged');
-            }
+            restoreSound.play(); // Play restore sound
+            enlargedImageContainer.remove();
         });
     });
 
     // Handle click for text description divs
     $('body').on('click', 'div.popupable', function () {
+        clickSound.play(); // Play click sound
+
         const textContent = $(this).text();
         const enlargedContent = $('<div>').addClass('enlarged-content');
         const closeButton = $('<span>').addClass('close-button').text('✖');
@@ -38,20 +31,9 @@ $(document).ready(function () {
         enlargedContent.append(closeButton);
         $('body').append(enlargedContent);
 
-        // Play click sound
-        clickSound.play();
-
-        // Add a class to indicate the enlarged state
-        enlargedContent.addClass('enlarged');
-
-        // Remove the class and play restore sound when clicked again
         closeButton.click(function () {
-            if (enlargedContent.hasClass('enlarged')) {
-                // Play restore sound
-                restoreSound.play();
-                // Remove the enlarged class
-                enlargedContent.removeClass('enlarged');
-            }
+            restoreSound.play(); // Play restore sound
+            enlargedContent.remove();
         });
     });
 });
