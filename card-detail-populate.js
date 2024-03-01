2f2203af9c48763cb9b98597b61ea281e141842c
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-03-01 22:56:30 +0530

    set click functionality to cardsn

--- card-detail-populate.js
+++ card-detail-populate.js
@@ -26,21 +26,23 @@ $(document).ready(function () {
         const container = $('#cardDetailContainer');
         container.empty(); // Clear existing content
 
-        // Create an image element
+        // Create an image container
+        const imageContainer = $('<div>').addClass('card-detail-image-container');
         const image = $('<img>').addClass('card-detail-image').attr('src', card.cx_image_url).attr('alt', 'Card Image');
+        imageContainer.append(image);
 
         // Create content container
         const contentDiv = $('<div>').addClass('card-detail-content');
 
         // Populate content dynamically
-        Object.keys(card).forEach(key => {
-            if (key !== 'cx_image_url') {
-                const element = $('<p>').addClass(`card-detail-${key}`).text(`${key}: ${card[key]}`);
-                contentDiv.append(element);
-            }
-        });
+        const nameDiv = $('<div>').addClass('card-detail-name').text(card.cx_name);
+        const taglineDiv = $('<div>').addClass('card-detail-tagline').text(card.cx_tagline);
+        const descriptionDiv = $('<div>').addClass('card-detail-description').text(card.cx_description);
 
-        container.append(image, contentDiv);
+        contentDiv.append(nameDiv, taglineDiv, descriptionDiv);
+
+        // Append image container and content div to the main container
+        container.append(imageContainer, contentDiv);
     }
 
     // Get card_id from query parameter
