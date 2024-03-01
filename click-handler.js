0a373c40c4f73003815d9b60000131b0e7ab31f7
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-03-02 00:01:16 +0530

    set click functionality to cardsn

--- click-handler.js
+++ click-handler.js
@@ -2,10 +2,11 @@ $(document).ready(function () {
     const clickSound = new Audio('resources/audio/deep_click_sound.mp3');
     const restoreSound = new Audio('resources/audio/fading_air_sound.mp3');
 
-
-
     // Handle click for card detail page on white spaces of the card
     $('body').on('click', '.card', function (event) {
+        // Play click sound
+        clickSound.play();
+
         // Extract the card_id from the clicked card
         const cardId = $(this).attr('data-card-id');
 
