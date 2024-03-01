a3d13a9d139de9d5ddea9932f6d1f5d61edcc516
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-03-01 23:28:08 +0530

    set click functionality to cardsn

--- card-detail-populate.js
+++ card-detail-populate.js
@@ -44,6 +44,31 @@ function displayCardDetails(card) {
     container.append(imageContainer, contentDiv);
 }
 
+// Function to handle image click
+$('body').on('click', '.card-detail-image', function () {
+    const imageUrl = $(this).attr('src');
+
+    // Create overlay container
+    const overlayContainer = $('<div>').addClass('image-overlay');
+    const overlayImage = $('<img>').addClass('overlay-image').attr('src', imageUrl).attr('alt', 'Enlarged Image');
+
+    overlayContainer.append(overlayImage);
+    $('body').append(overlayContainer);
+
+    // Add a class to indicate the enlarged state
+    overlayContainer.addClass('enlarged');
+
+    // Handle a second click on the enlarged image to restore
+    overlayContainer.one('click', function () {
+        // Remove the 'enlarged' class after the second click
+        overlayContainer.removeClass('enlarged');
+        // Remove the overlay after the restore animation
+        setTimeout(function () {
+            overlayContainer.remove();
+        }, 500);
+    });
+});
+
 // Get card_id from query parameter
 const urlParams = new URLSearchParams(window.location.search);
 const cardId = urlParams.get('card_id');
