e7fed463feb56c88b300610ef804e9d6b72fe3f0
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-03-02 03:46:12 +0530

    set click functionality to cardsn

--- fetchCategory.js
+++ fetchCategory.js
@@ -1,21 +1,3 @@
-// Function to fetch cards by category from the API
-function fetchCardsByCategory(category) {
-    const noOfCards = 10; // Default number of cards
-
-    // Make AJAX request to the API with category filter
-    $.ajax({
-        url: `${API_BASE_URL}:5000/getCards`,
-        type: 'GET',
-        data: { type: category, noOfCards: noOfCards },
-        success: function (response) {
-            displayCards(response.cards);
-        },
-        error: function (error) {
-            console.log(`Error fetching ${category} cards:`, error);
-        }
-    });
-}
-
 // fetchCategories.js
 async function fetchCategories() {
     const categoriesResponse = await fetch('http://3.26.65.147:5000/getCategories', { method: 'GET' });
@@ -25,7 +7,7 @@ async function fetchCategories() {
     categories.forEach(category => {
         const tab = document.createElement('button');
         tab.innerText = category; // Assuming the category object has a 'name' property
-        tab.onclick = () => fetchCardsByCategory(category);
+        tab.onclick = () => fetchProductsByCategory(category);
         categoryTabsContainer.appendChild(tab);
     });
 }
@@ -38,7 +20,7 @@ function displayCategories(categories) {
     categories.forEach(category => {
         const tab = document.createElement('button');
         tab.innerText = category;
-        tab.onclick = () => fetchCardsByCategory(category);
+        tab.onclick = () => fetchProductsForCategory(category);
         categoryTabsContainer.appendChild(tab);
     });
     document.body.insertBefore(categoryTabsContainer, document.getElementById('product-details'));
