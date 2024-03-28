61ac99d80ad82fdd69bb189dde198336c828c120
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-03-28 21:18:28 +0530

    set all ips to one single location

--- api-config.js
+++ api-config.js
@@ -1,3 +1,3 @@
 // api-config.js
-const API_BASE_URL = 'http://3.26.74.195/'; // Replace with the actual IP address
-window.API_BASE_URL = 'http://3.26.74.195/'; // Define the variable in the global scope
+//const API_BASE_URL = 'http://3.26.74.195'; // Replace with the actual IP address
+window.API_BASE_URL = 'http://3.26.74.195'; // Define the variable in the global scope
