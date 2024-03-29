a4387ff13612ec5a1726874a019b07fc3a90ba2a
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-03-30 01:07:27 +0530

    revert not

--- api-config.js
+++ api-config.js
@@ -1,3 +1,3 @@
 // api-config.js
-const API_BASE_URL = 'http://13.210.38.219'; // Replace with the actual IP address
-window.API_BASE_URL = 'http://13.210.38.219'; // Define the variable in the global scope
+const API_BASE_URL = 'http://3.27.49.123'; // Replace with the actual IP address
+window.API_BASE_URL = 'http://3.27.49.123'; // Define the variable in the global scope
