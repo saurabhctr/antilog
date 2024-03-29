d82b0f463313890a8abcbb8e440cad0c20b05e2e
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-03-29 23:13:38 +0530

    revert not

--- success-page.js
+++ success-page.js
@@ -2,6 +2,9 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     // Go to Dashboard button click event
+    particlesJS.load('particles-js', 'particlesjs-config.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
     document.getElementById('goToDashboardBtn').addEventListener('click', function() {
         // Navigate to the home page (replace 'index.html' with your actual home page URL)
         window.location.href = 'index.html';
