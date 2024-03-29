d4fc3e3c64776d40e66fa49d0601af556fc730b1
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-03-29 21:24:27 +0530

    revert not

--- success-page.js
+++ success-page.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Go to Dashboard button click event
     document.getElementById('goToDashboardBtn').addEventListener('click', function() {
         // Navigate to the home page (replace 'index.html' with your actual home page URL)
-        //window.location.href = 'index.html';
+        window.location.href = 'index.html';
     });
 });
