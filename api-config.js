4a4c60569662b306a15663d22254cdb24abba890
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-04-20 00:55:03 +0530

    add bokeh server embeddings RESTART IT

--- api-config.js
+++ api-config.js
@@ -1,3 +1,3 @@
 // api-config.js
-const API_BASE_URL = 'http://52.64.92.202/'; // Replace with the actual IP address
-window.API_BASE_URL = 'http://52.64.92.202/'; // Define the variable in the global scope
+const API_BASE_URL = 'http://52.64.92.202'; // Replace with the actual IP address
+window.API_BASE_URL = 'http://52.64.92.202'; // Define the variable in the global scope
