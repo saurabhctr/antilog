7e598dfb8a7c44f456d2c8439979f822ad748ff5
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-04-24 22:49:45 +0530

    secure https applied

--- api-config.js
+++ api-config.js
@@ -1,3 +1,3 @@
 // api-config.js
-const API_BASE_URL = 'https://3.25.62.84'; // Replace with the actual IP address
-window.API_BASE_URL = 'https://3.25.62.84'; // Define the variable in the global scope
+const API_BASE_URL = 'https://3.27.25.83'; // Replace with the actual IP address
+window.API_BASE_URL = 'https://3.27.25.83'; // Define the variable in the global scope
