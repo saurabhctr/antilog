1bf97d30b2a7451444bb691a3121651252f946f5
saurabhctr <sacruiseewall.cruise@gmail.com>
2024-04-28 00:48:49 +0530

    clock fix

--- api-config.js
+++ api-config.js
@@ -1,3 +1,3 @@
 // api-config.js
-const API_BASE_URL = 'https://3.27.25.83'; // Replace with the actual IP address
-window.API_BASE_URL = 'https://3.27.25.83'; // Define the variable in the global scope
+const API_BASE_URL = 'https://3.25.238.94'; // Replace with the actual IP address
+window.API_BASE_URL = 'https://3.25.238.94'; // Define the variable in the global scope
