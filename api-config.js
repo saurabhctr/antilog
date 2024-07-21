30fb5456b12502b6ff2b30f75ef12c81c61e21d8
saurabh <saurabhkr609@gmail.com>
2024-07-21 18:57:29 +0000

    test shoft to my comp final

--- api-config.js
+++ api-config.js
@@ -1,3 +1,3 @@
 // api-config.js
-const API_BASE_URL = 'https://13.54.208.219 '; // Replace with the actual IP address
-window.API_BASE_URL = 'https://13.54.208.219 '; // Define the variable in the global scope
+const API_BASE_URL = 'https://13.54.208.219'; // Replace with the actual IP address
+window.API_BASE_URL = 'https://13.54.208.219'; // Define the variable in the global scope
